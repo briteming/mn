75a17191a27312fda14bcf93eb290ac26b19e9d7
briteming <briteming@gmail.com>
2025-12-23 23:28:04 -0800

    Update markdownToHtml.ts

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -5,7 +5,7 @@ import gfm from "remark-gfm";
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(gfm)
-    .use(html)
+    .use(html, { sanitize: false })
     .process(markdown);
   return result.toString();
 }
